4d53238dbfdc5e01e6a1c890464566d3750d5aba
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-11-23 15:59:57 -0600

    Fix: inital arguments

--- run.py
+++ run.py
@@ -1,22 +1,22 @@
-def merge(list):
+def merge(list_1, list_2):
     initial_sorted_list = []
     i = 0
     j = 0
 
-    while i < len(list) and j < len(list):
-        if list[i] < list[j]:
-            initial_sorted_list.append(list[i])
+    while i < len(list_1) and j < len(list_2):
+        if list_1[i] < list_2[j]:
+            initial_sorted_list.append(list_1[i])
             i += 1
         else:
-            initial_sorted_list.append(list[j])
+            initial_sorted_list.append(list_2[j])
             j += 1
 
-    while i < list[i]:
-        initial_sorted_list.append(list[i])
+    while i < list_1[i]:
+        initial_sorted_list.append(list_1[i])
         i += 1
 
-    while j < list[j]:
-        initial_sorted_list.append(list[j])
+    while j < list_2[j]:
+        initial_sorted_list.append(list_2[j])
         j += 1
 
     return initial_sorted_list
