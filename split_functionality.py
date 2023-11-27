33b52bf750c38e395451de5961b8ff2f60eec846
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-11-26 20:02:10 -0600

    Add: function string, fix formatting

--- split_functionality.py
+++ split_functionality.py
@@ -1,6 +1,14 @@
 def split_list(initial_list):
+    """
+    Splits a list into 2 parts
+    Args:
+        initial_list (list): list of numbers
+    Returns:
+        left_part (list): left part of the list
+        right_part (list): right part of the list
+    """
     mid_point = initial_list // 2
     left_part = initial_list[:mid_point]
     right_part = initial_list[mid_point:]
 
-    return left_part, right_part 
+    return left_part, right_part
