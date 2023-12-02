e32a266af5b90d0f95d5a0ddac66ece5603c52e9
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-02 14:03:21 -0600

    Add merge_sort separately

---
+++ merge_sort.py
@@ -0,0 +1,28 @@
+from split_functionality import split
+from merge_functionality import merge
+
+
+def merge_sort(list):
+    """
+    Sorts a list in ascending order
+    Returns a new sorted list
+
+    Divide: Find the midpoint of the list and divide into sublists
+    Conquer: Recursively sort the sublists created in previous step
+    Combine: Merge the sorted sublists created in previous step
+
+    Takes O(kn log n) time
+    """
+
+    if len(list) <= 1:
+        return list
+
+    left_half, right_half = split(list)
+    print("left_half", left_half)
+    print("right_half", right_half)
+    left = merge_sort(left_half)
+    print("left", left)
+    right = merge_sort(right_half)
+    print("right", right)
+
+    return merge(left, right)
