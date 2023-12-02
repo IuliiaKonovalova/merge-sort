5135c5ddf27f089f4ec0a0caa49b42e01282635d
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-02 14:29:00 -0600

    Add comments strings

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ from test_cases import test_merge_sort
 
 
 def main():
+    """
+    Runs the merge sort algorithm implementation
+    Asks user for a list of comma separated numbers
+    """
     input("Press Enter to continue...")
     # let user input different numbers
     numbers = input("Enter numbers separated by a comma: ")
