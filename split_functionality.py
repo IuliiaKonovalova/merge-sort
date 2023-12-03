1dd3e4e737c29bc2532b3167476233fc00af6463
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-02 18:34:38 -0600

    Add colorfull split

--- split_functionality.py
+++ split_functionality.py
@@ -1,3 +1,6 @@
+from constant_dictionary import constants
+
+
 def split_list(initial_list):
     """
     Splits a list into 2 parts
@@ -7,8 +10,18 @@ def split_list(initial_list):
         left_part (list): left part of the list
         right_part (list): right part of the list
     """
+    input("Enter split_list Press Enter to continue...")
     mid_point = len(initial_list) // 2
+    print("mid_point", mid_point)
     left_part = initial_list[:mid_point]
+    print("left_part: ", left_part)
+    for key in left_part:
+        print(constants[key], end="")
+    print("\n")
     right_part = initial_list[mid_point:]
+    print("right_part: ", right_part)
+    for key in right_part:
+        print(constants[key], end="")
+    print("\n")
 
     return left_part, right_part
