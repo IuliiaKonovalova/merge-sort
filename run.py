76421ea6770afa1f50fada8bcea956b665f63466
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-03 12:09:02 -0600

    Add simple terminal menu functionality

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from string_to_list import string_to_list
-from test_cases import test_merge_sort
+from simple_term_menu import TerminalMenu
+# from string_to_list import string_to_list
+from test_cases import test_merge_sort_with_constants
+from constant_dictionary import *
 
 
 def main():
@@ -9,11 +11,45 @@ def main():
     """
     input("Press Enter to continue...")
     # let user input different numbers
-    numbers = input("Enter numbers separated by a comma: ")
+    # numbers = input("Enter numbers separated by a comma: ")
     # convert string to list
-    numbers = string_to_list(numbers)
-    test_merge_sort(numbers)
+    # numbers = string_to_list(numbers)
+    # test_merge_sort(numbers)
 
+    options = ['1. Merge Sort', '2. Selection Sort', '3. Insertion Sort', '4. Bubble Sort', '5. Quit']
+    main_menu = TerminalMenu(options)
+    sub_options = [
+        '1. "Yellow to red"',
+        '2. "Short version"',
+        '3. "Full version"',
+    ]
+    sub_menu = TerminalMenu(sub_options)
+    quitting = False
+    while quitting is not True:
+        options_index = main_menu.show()
+        options_choice = options[options_index]
+        if options_choice == '5. Quit':
+            print(f'''QUITTING...''')
+            quitting = True
+        elif options_choice == '1. Merge Sort':
+            print(f'''MERGE SORT''')
+            sub_index = sub_menu.show()
+            sub_choice = sub_options[sub_index]
+            if sub_choice == '1. "Yellow to red"':
+                print(f'''YELLOW TO RED''')
+                test_merge_sort_with_constants(constants_1)
+            elif sub_choice == '2. "Short version"':
+                print(f'''SHORT VERSION''')
+                test_merge_sort_with_constants(constants_2)
+            elif sub_choice == '3. "Full version"':
+                print(f'''FULL VERSION''')
+                test_merge_sort_with_constants(constants_3)
+        elif options_choice == '2. Selection Sort':
+            print(f'''SELECTION SORT''')
+        elif options_choice == '3. Insertion Sort':
+            print(f'''INSERTION SORT''')
+        elif options_choice == '4. Bubble Sort':
+            print(f'''BUBBLE SORT''')
 
 if __name__ == "__main__":
     main()
