c9402dd8bfbf6216273f4524eb9b01808188666d
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-04 17:13:24 -0600

    fix: selection code logic

--- selection_sort.py
+++ selection_sort.py
@@ -1,28 +1,42 @@
-from print_sorted_key import print_sorted_keys
-from test_cases import generate_unsorted_list_from_constants
+import time
+import os
+from tabulate import tabulate
+from print_items import print_items
 from constant_dictionary import *
 
 
-import time
-import os
 
 def selection_sort(list_data, constants):
-
+    headers = ["scan_index", "1st POINTER", "2nd POINTER"]
+    
     for scan_index in range(0, len(list_data)):
+        table_data = []
         min_index = scan_index
         print("1st POINTER: ", min_index)
         for comp_index in range(scan_index + 1, len(list_data)):
-            print("2nd POINTER: ", comp_index)
-            if list_data[comp_index] < list_data[min_index]:
-                min_index = comp_index
-                print("1st POINTER Update: ", min_index)
-        if min_index != scan_index:
-            list_data[scan_index], list_data[min_index] = list_data[min_index], list_data[scan_index]
             input("Enter Press Enter to continue...")
             os.system('clear')
+            print("\n LIST DATA: ", list_data)
+            print("2nd POINTER: ", comp_index)
+            table_data.append([scan_index, min_index, comp_index])
+            print(tabulate(table_data, headers=headers))
+            if list_data[comp_index] < list_data[min_index]:
+                min_index = comp_index
+                input("Enter Press Enter to continue...")
+                os.system('clear')
+                print("\n LIST DATA: ", list_data)
+                print("1st POINTER Update: ", min_index)
+                table_data.append([scan_index, min_index, comp_index])
+                print(tabulate(table_data, headers=headers))
+        if min_index != scan_index:
+            list_data[scan_index], list_data[min_index] = list_data[min_index], list_data[scan_index]
+
+
             print("\n")
             print("LIST DATA AFTER SORT: ", list_data)
-            print_sorted_keys(list_data, constants)
+            print_items(list_data, constants)
+            table_data.append([scan_index, min_index, comp_index])
+            print(tabulate(table_data, headers=headers))
             time.sleep(0.9)
 
     return list_data
