a46aca0e7c3b974774892f3e5f081be9299c0042
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-05 11:36:51 -0600

    ref: clean up code

--- print_items.py
+++ print_items.py
@@ -1,5 +1,4 @@
 import random
-# from constant_dictionary import constants
 
 
 def print_items(keys, constants_dict):
@@ -11,17 +10,11 @@ def print_items(keys, constants_dict):
             print(" ", end="")
         print(key, end=": ")
         normalized_key = int(((key + 1) / max_key) * max_key)
-        # normalized_key = int((key / max_key) * (max_key + 5))  # Normalizing key for bar length
         color_code = constants_dict[key]
         print(color_code * normalized_key)
 
-# keys_to_sort1 = list(range(len(constants)))
-# keys_to_sort = random.sample(keys_to_sort1, len(keys_to_sort1))
 
-
-# print one item based on the key
 def print_temp_item(key, list_data, constants_dict):
-    # get max key from constants_dict
     max_key = max(list_data)
     if key == 0:
         key = 1
