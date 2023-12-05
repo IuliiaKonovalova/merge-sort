7652149180eda7c660311ca79d780bcfc4112816
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-05 17:04:52 -0600

    feat: add insertion main theory functionality

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,9 +4,6 @@ from tabulate import tabulate
 from print_items import print_items
 
 
-unordered_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
 def insertion_sort(unordered_list):
     for scan_index in range(1, len(unordered_list)):
         temp = unordered_list[scan_index]
@@ -18,6 +15,24 @@ def insertion_sort(unordered_list):
     return unordered_list
 
 
-# insertion_sort(unordered_list)
-
-
+def insertion_sort(list_data, constants):
+    for scan_index in range(1, len(list_data)):
+        print("SCAN_INDEX: ", scan_index)
+        temp = list_data[scan_index]
+        print("TEMP: ", temp)
+        min_index = scan_index
+        print("MIN_INDEX: ", min_index)
+        while min_index > 0 and temp < list_data[min_index - 1]:
+            list_data[min_index] = list_data[min_index - 1]
+            print("ITEM AT MIN INDEX: ", list_data[min_index])
+            min_index -= 1
+            print("MIN_INDEX UPDATED: ", min_index)
+            list_data[min_index] = temp
+            print("ITEM AT MIN INDEX FROM TEMP: ", list_data[min_index])
+        input("Enter Press Enter to continue...")
+        os.system('clear')
+        print("\n")
+        print("LIST DATA AFTER SORT: ", list_data)
+        print_items(list_data, constants)
+        time.sleep(0.9)
+    return list_data
