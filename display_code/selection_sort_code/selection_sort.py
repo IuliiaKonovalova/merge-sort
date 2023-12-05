fe1adc46f4a6d6b3197708f3bf27cccbb7b1d0a4
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-05 11:38:23 -0600

    add: selection sort code print functionality

---
+++ display_code/selection_sort_code/selection_sort.py
@@ -0,0 +1,17 @@
+from colorama import Fore, Back, Style
+
+
+def display_selection_sort_code():
+    print(
+        f"""
+{Fore.BLUE}def {Fore.WHITE} selection_sort{Fore.YELLOW}({Fore.WHITE}list_data, constants{Fore.YELLOW}){Fore.WHITE}:
+    {Fore.BLUE}for {Fore.WHITE} scan_index in range{Fore.YELLOW}({Fore.WHITE}0, len{Fore.YELLOW}({Fore.WHITE}list_data{Fore.YELLOW})){Fore.WHITE}:
+        min_index = scan_index
+        {Fore.BLUE}for{Fore.WHITE} comp_index in range{Fore.YELLOW}({Fore.WHITE}scan_index + 1, len{Fore.YELLOW}({Fore.WHITE}list_data{Fore.YELLOW})){Fore.WHITE}:
+            {Fore.BLUE}if{Fore.WHITE} list_data{Fore.YELLOW}[{Fore.WHITE}comp_index{Fore.YELLOW}]{Fore.WHITE} < list_data{Fore.YELLOW}[{Fore.WHITE}min_index{Fore.YELLOW}]{Fore.WHITE}:
+                min_index = comp_index
+        {Fore.BLUE}if{Fore.WHITE} min_index != scan_index:
+            list_data{Fore.YELLOW}[{Fore.WHITE}scan_index{Fore.YELLOW}]{Fore.WHITE}, list_data{Fore.YELLOW}[{Fore.WHITE}min_index{Fore.YELLOW}]{Fore.WHITE} = list_data{Fore.YELLOW}[{Fore.WHITE}min_index{Fore.YELLOW}]{Fore.WHITE}, list_data{Fore.YELLOW}[{Fore.WHITE}scan_index{Fore.YELLOW}]{Fore.WHITE}
+    {Fore.BLUE}return{Fore.WHITE} list_data
+    """
+    )
