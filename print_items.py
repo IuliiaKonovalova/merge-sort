71fe2389f2b7c814c8e811c93e73cd765c09615c
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-05 11:35:52 -0600

    add: printing graphs item functionality

--- print_items.py
+++ print_items.py
@@ -18,3 +18,15 @@ def print_items(keys, constants_dict):
 # keys_to_sort1 = list(range(len(constants)))
 # keys_to_sort = random.sample(keys_to_sort1, len(keys_to_sort1))
 
+
+# print one item based on the key
+def print_temp_item(key, list_data, constants_dict):
+    # get max key from constants_dict
+    max_key = max(list_data)
+    if key == 0:
+        key = 1
+    if key < 10:
+        print(" ", end="")
+    print(key, end=": ")
+    normalized_key = int(((key + 1) / max_key) * max_key)
+    print(constants_dict[key] * normalized_key)
