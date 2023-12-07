e465c0112f81d4891111a18ffdbb83c3785e988e
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-07 17:27:55 -0600

    add: call to selection sort animation and print results functionality

--- test_cases.py
+++ test_cases.py
@@ -68,3 +68,18 @@ def show_selection_sort_with_constants(constants):
     )
 
 
+def show_selection_sort_animation_with_constants(constants):
+    """
+    Tests the merge sort algorithm with the constants dictionary
+    """
+    unsorted_list = generate_unsorted_list_from_constants(constants)
+    temp_list = unsorted_list.copy()
+    print(
+        f"""
+    To compare:
+    Sorted list: {selection_sort_animation(unsorted_list, constants)}
+    Initial list: {temp_list}
+    """
+    )
+
+
