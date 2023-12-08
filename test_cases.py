c4e36f8987eab2e6f2ef8182d3c32c2047b47506
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-08 17:49:07 -0600

    chore: add printing algorythim name

--- test_cases.py
+++ test_cases.py
@@ -7,9 +7,11 @@ from animations.merge_sort_animation.merge_sort import *
 from insertion_sort import insertion_sort
 from animations.insertion_sort_animation.insertion_sort import *
 from bubble_sort import bubble_sort
+from animations.bubble_sort_animation.bubble_sort import *
+from constant_dictionary import constants_1
 
 
-def generate_unsorted_list_from_constants(constants):
+def generate_unsorted_list_from_constants(constants, sort_algorithm_name):
     """
     Generates an unsorted list of numbers
     :return: an unsorted list of numbers
@@ -18,9 +20,9 @@ def generate_unsorted_list_from_constants(constants):
     for key in constants:
         unsorted_list.append(key)
     random.shuffle(unsorted_list)
+    print(sort_algorithm_name)
+    print(unsorted_list)
     print_items(unsorted_list, constants)
-    print("\n")
-    print("RANDOM LIST: ", unsorted_list)
     return unsorted_list
 
 
@@ -28,7 +30,8 @@ def show_merge_sort_with_constants(constants):
     """
     Tests the merge sort algorithm with the constants dictionary
     """
-    unsorted_list = generate_unsorted_list_from_constants(constants)
+    sort_algorithm_name = "MERGE SORT"
+    unsorted_list = generate_unsorted_list_from_constants(constants, sort_algorithm_name)
     temp_list = unsorted_list.copy()
     print(
     f"""
@@ -43,7 +46,8 @@ def show_merge_sort_animation_with_constants(constants):
     """
     Tests the merge sort algorithm with the constants dictionary
     """
-    unsorted_list = generate_unsorted_list_from_constants(constants)
+    sort_algorithm_name = "MERGE SORT"
+    unsorted_list = generate_unsorted_list_from_constants(constants, sort_algorithm_name)
     temp_list = unsorted_list.copy()
     print(
     f"""
@@ -58,7 +62,8 @@ def show_selection_sort_with_constants(constants):
     """
     Tests the merge sort algorithm with the constants dictionary
     """
-    unsorted_list = generate_unsorted_list_from_constants(constants)
+    sort_algorithm_name = "SELECTION SORT"
+    unsorted_list = generate_unsorted_list_from_constants(constants, sort_algorithm_name)
     temp_list = unsorted_list.copy()
     print(
         f"""
@@ -73,7 +78,8 @@ def show_selection_sort_animation_with_constants(constants):
     """
     Tests the merge sort algorithm with the constants dictionary
     """
-    unsorted_list = generate_unsorted_list_from_constants(constants)
+    sort_algorithm_name = "SELECTION SORT"
+    unsorted_list = generate_unsorted_list_from_constants(constants, sort_algorithm_name)
     temp_list = unsorted_list.copy()
     print(
         f"""
@@ -88,8 +94,9 @@ def show_insertion_sort_with_constants(constants):
     """
     Tests the merge sort algorithm with the constants dictionary
     """
+    sort_algorithm_name = "INSERTION SORT"
     temp_list = unsorted_list.copy()
-    unsorted_list = generate_unsorted_list_from_constants(constants)
+    unsorted_list = generate_unsorted_list_from_constants(constants, sort_algorithm_name)
     print("Sorted list: ", insertion_sort(unsorted_list, constants))
 
 
@@ -97,7 +104,8 @@ def show_insertion_sort_animation_with_constants(constants):
     """
     Tests the merge sort algorithm with the constants dictionary
     """
-    unsorted_list = generate_unsorted_list_from_constants(constants)
+    sort_algorithm_name = "INSERTION SORT"
+    unsorted_list = generate_unsorted_list_from_constants(constants, sort_algorithm_name)
     temp_list = unsorted_list.copy()
     print(
         f"""
@@ -112,7 +120,8 @@ def show_bubble_sort_with_constants(constants):
     """
     Tests the merge sort algorithm with the constants dictionary
     """
-    unsorted_list = generate_unsorted_list_from_constants(constants)
+    sort_algorithm_name = "BUBBLE SORT"
+    unsorted_list = generate_unsorted_list_from_constants(constants, sort_algorithm_name)
     temp_list = unsorted_list.copy()
     print(
         f"""
@@ -121,3 +130,4 @@ def show_bubble_sort_with_constants(constants):
     Initial list: {temp_list}
     """
     )
+
