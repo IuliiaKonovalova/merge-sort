3c4dd69b5da18dda4d221c15bd7cecf4c5f896af
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-08 16:35:24 -0600

    feat: add insertion sort test case animation

--- run.py
+++ run.py
@@ -107,7 +107,7 @@ def main():
         elif options_choice == '3. Insertion Sort':
             print(f'''INSERTION SORT''')
             show_insertion_sort_with_constants(constants_1)
-            
+            show_insertion_sort_animation_with_constants(constants_3)
         elif options_choice == '4. Bubble Sort':
             print(f'''BUBBLE SORT''')
 
