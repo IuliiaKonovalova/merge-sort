5a3f115165415cac48ab68a297d27c0dfed4beb0
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-07 18:19:37 -0600

    feat: add printing with highlight

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,10 @@
-import time
+from time import sleep
 import os
-from print_items import print_items
+from print_items import *
+from colorama import Fore, Back, Style
 
-unorder_list  = [3, 1, 4]
 
-def bubble_sort(unord_list):
+def bubble_sort(unordered_list, constants):
     """
     Sorts a list of numbers using bubble sort algorithm
     Args:
@@ -12,9 +12,23 @@ def bubble_sort(unord_list):
     Returns:
         initial_list (list): sorted list of numbers
     """
-    for i in range(len(unord_list) - 1):
-        for j in range(len(unord_list) - 1):
-            if unord_list[j] > unord_list[j + 1]:
-                unord_list[j], unord_list[j + 1] = unord_list[j + 1], unord_list[j]
+    for i in range(len(unordered_list) - 1):
+        print(f"""Index in i:  {Fore.GREEN}{i}{Fore.WHITE}""")
+        for j in range(len(unordered_list) - 1):
+            print(f"""Index in j:  {Fore.BLUE}{j}{Fore.WHITE}""")
 
-    return unord_list
+            if unordered_list[j] > unordered_list[j + 1]:
+                print_temp_item(unordered_list[j], unordered_list, constants)
+                print_temp_item(unordered_list[j + 1], unordered_list, constants)
+
+                unordered_list[j], unordered_list[
+                    j + 1
+                ] = unordered_list[j + 1], unordered_list[j]
+                print("Swapping values")
+                print_temp_item(unordered_list[j], unordered_list, constants)
+                print_temp_item(unordered_list[j + 1], unordered_list, constants)
+                print("\n")
+                print("LIST now: ", unordered_list)
+                print_items(unordered_list, constants)
+
+    return unordered_list
