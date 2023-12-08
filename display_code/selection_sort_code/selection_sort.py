c013c847e50554a0c61a5c5a56e9a2487533b3c5
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-08 16:54:17 -0600

    chose: fix indentation in the print functiona

--- display_code/selection_sort_code/selection_sort.py
+++ display_code/selection_sort_code/selection_sort.py
@@ -1,11 +1,11 @@
-from colorama import Fore, Back, Style
+from colorama import Fore
 
 
 def display_selection_sort_code():
     print(
         f"""
-{Fore.BLUE}def {Fore.WHITE} selection_sort{Fore.YELLOW}({Fore.WHITE}list_data, constants{Fore.YELLOW}){Fore.WHITE}:
-    {Fore.BLUE}for {Fore.WHITE} scan_index in range{Fore.YELLOW}({Fore.WHITE}0, len{Fore.YELLOW}({Fore.WHITE}list_data{Fore.YELLOW})){Fore.WHITE}:
+{Fore.BLUE}def{Fore.WHITE} selection_sort{Fore.YELLOW}({Fore.WHITE}list_data, constants{Fore.YELLOW}){Fore.WHITE}:
+    {Fore.BLUE}for {Fore.WHITE}scan_index in range{Fore.YELLOW}({Fore.WHITE}0, len{Fore.YELLOW}({Fore.WHITE}list_data{Fore.YELLOW})){Fore.WHITE}:
         min_index = scan_index
         {Fore.BLUE}for{Fore.WHITE} comp_index in range{Fore.YELLOW}({Fore.WHITE}scan_index + 1, len{Fore.YELLOW}({Fore.WHITE}list_data{Fore.YELLOW})){Fore.WHITE}:
             {Fore.BLUE}if{Fore.WHITE} list_data{Fore.YELLOW}[{Fore.WHITE}comp_index{Fore.YELLOW}]{Fore.WHITE} < list_data{Fore.YELLOW}[{Fore.WHITE}min_index{Fore.YELLOW}]{Fore.WHITE}:
