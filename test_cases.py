7fadac96f31ad81a09fbb2355517c555d9e9adc7
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-08 17:51:10 -0600

    feat: add bubble animation test case

--- test_cases.py
+++ test_cases.py
@@ -137,3 +137,18 @@ def show_bubble_sort_with_constants(constants):
     """
     )
 
+
+def show_bubble_sort_animation_with_constants(constants):
+    """
+    Tests the merge sort algorithm with the constants dictionary
+    """
+    sort_algorithm_name = "BUBBLE SORT"
+    unsorted_list = generate_unsorted_list_from_constants(constants, sort_algorithm_name)
+    temp_list = unsorted_list.copy()
+    print(
+        f"""
+    To compare:
+    Sorted list: {bubble_sort_animation(unsorted_list, constants)}
+    Initial list: {temp_list}
+    """
+    )
