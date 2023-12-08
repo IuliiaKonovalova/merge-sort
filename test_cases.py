bb64d48ecba5069e8e58eca26b305c510f52e0b2
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-08 16:37:23 -0600

    feat: add insertion sort test case animation

--- test_cases.py
+++ test_cases.py
@@ -6,6 +6,7 @@ from print_items import print_items
 from animations.merge_sort_animation.merge_sort import *
 from insertion_sort import insertion_sort
 from animations.insertion_sort_animation.insertion_sort import *
+from bubble_sort import bubble_sort
 
 
 def generate_unsorted_list_from_constants(constants):
@@ -81,5 +82,31 @@ def show_selection_sort_animation_with_constants(constants):
     Initial list: {temp_list}
     """
     )
+
+
+def show_bubble_sort_with_constants(constants):
+    """
+    Tests the merge sort algorithm with the constants dictionary
+    """
+    unsorted_list = generate_unsorted_list_from_constants(constants)
+    temp_list = unsorted_list.copy()
+    print(
+        f"""
+    To compare:
+    Sorted list: {bubble_sort(unsorted_list, constants)}
+    Initial list: {temp_list}
+    """
+    )
+
+
+def show_insertion_sort_animation_with_constants(constants):
+    """
+    Tests the merge sort algorithm with the constants dictionary
+    """
+    unsorted_list = generate_unsorted_list_from_constants(constants)
+    temp_list = unsorted_list.copy()
+    print("insertion_sort_animation: ")
+    print("Sorted list: ", insertion_sort_animation(unsorted_list, constants))
+    print("Initial list: ", temp_list)
 
 
