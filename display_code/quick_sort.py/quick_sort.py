a188b7943e6d00ff7448dfb135a697f692eee621
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-10 00:43:41 -0600

    restructure: set up print formula

--- display_code/quick_sort.py/quick_sort.py
+++ display_code/quick_sort.py/quick_sort.py
@@ -1,23 +1,17 @@
+from colorama import Fore
 
 
-# Example usage:
-# my_list = [12, 4, 5, 6, 7, 3, 1, 15]
-# sorted_list = quick_sort(my_list)
-# print(sorted_list)
-
-
-def quick_sort(arr):
-    if len(arr) <= 1:  # Base case: If the array has 0 or 1 element, it is already sorted
-        return arr
-    else:
-        pivot = arr[0]  # Choosing the first element as the pivot
-        less_than_pivot = [x for x in arr[1:] if x <= pivot]  # Elements less than or equal to pivot
-        greater_than_pivot = [x for x in arr[1:] if x > pivot]  # Elements greater than pivot
-        
-        # Recursively apply quick sort to partitions and concatenate results
-        return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
-
-# Example usage:
-my_list = [12, 4, 5, 6, 7, 3, 1, 15]
-sorted_list = quick_sort(my_list)
-print(sorted_list)
+def display_quick_sort_code():
+    print(
+        f"""
+{Fore.BLUE}def{Fore.WHITE} quick_sort{Fore.YELLOW}({Fore.WHITE}arr{Fore.YELLOW}){Fore.WHITE}:
+    {Fore.BLUE}if{Fore.WHITE} len{Fore.YELLOW}({Fore.WHITE}arr{Fore.YELLOW}){Fore.WHITE} <= 1:
+        {Fore.BLUE}return{Fore.WHITE} arr
+    {Fore.BLUE}else{Fore.WHITE}:
+        pivot = arr{Fore.YELLOW}[{Fore.WHITE}[0{Fore.YELLOW}]{Fore.WHITE}
+        less_than_pivot = {Fore.YELLOW}[{Fore.WHITE}x for x in arr{Fore.YELLOW}[{Fore.WHITE}1:{Fore.YELLOW}]{Fore.WHITE} if x <= pivot{Fore.YELLOW}]{Fore.WHITE}
+        greater_than_pivot = {Fore.YELLOW}[{Fore.WHITE}x for x in arr{Fore.YELLOW}[{Fore.WHITE}1:{Fore.YELLOW}]{Fore.WHITE} if x > pivot{Fore.YELLOW}]{Fore.WHITE}
+        {Fore.BLUE}return{Fore.WHITE} quick_sort{Fore.YELLOW}({Fore.WHITE}less_than_pivot{Fore.YELLOW}){Fore.WHITE} + {Fore.YELLOW}[{Fore.WHITE}pivot{Fore.YELLOW}]{Fore.WHITE} + quick_sort{Fore.YELLOW}({Fore.WHITE}greater_than_pivot{Fore.YELLOW}){Fore.WHITE}
+    """
+    )
+display_quick_sort_code()
