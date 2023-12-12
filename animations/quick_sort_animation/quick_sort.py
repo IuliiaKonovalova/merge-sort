6054f46fadbf56044a954d4bc6e8bf43947b8dba
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-11 20:18:33 -0600

    feat: add animation for quick sort

---
+++ animations/quick_sort_animation/quick_sort.py
@@ -0,0 +1,55 @@
+import os
+from time import sleep
+from print_items import print_items, print_temp_item  # Assuming these functions help print the items
+from constant_dictionary import *  # Assuming this contains constant values
+
+def quick_sort_animation(arr, constants):
+    """
+    Sorts a list of numbers using quick sort algorithm
+    Args:
+        arr (list): list of numbers
+    Returns:
+        arr (list): sorted list of numbers
+    """
+    quick_sort_recursive(arr, 0, len(arr) - 1, constants)
+    return arr
+
+def quick_sort_recursive(arr, low, high, constants):
+    """
+    Recursive function for quick sort
+    """
+    if low < high:
+        pivot_index = partition(arr, low, high, constants)
+        quick_sort_recursive(arr, low, pivot_index - 1, constants)
+        quick_sort_recursive(arr, pivot_index + 1, high, constants)
+
+def partition(arr, low, high, constants):
+    """
+    Partitioning step in quick sort
+    """
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+            printing_animation(arr, pivot, i, j, low, high, constants)
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    printing_animation(arr, pivot, i + 1, high, low, high, constants)
+    return i + 1
+
+def printing_animation(arr, pivot, pivot_index, current_index, low, high, constants):
+    """ Prints the animation of quick sort """
+    os.system('clear')
+    print("QUICK SORT ANIMATION")
+    print("\n Pivot: ", pivot)
+    print("\n Current Index: ", current_index)
+    print("\n Low: ", low)
+    print("\n High: ", high)
+    print_items(arr, constants)
+    sleep(0.9)
+
+# Example usage:
+my_list = [12, 4, 5, 6, 7, 3, 1, 15]
+sorted_list = quick_sort_animation(my_list, your_constants)
+print(sorted_list)
