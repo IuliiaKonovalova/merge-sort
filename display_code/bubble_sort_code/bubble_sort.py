ddf0013519af2157d5cad9e77fa83ac21da660a8
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-12 16:51:03 -0600

    chore: replace colorama variables

--- display_code/bubble_sort_code/bubble_sort.py
+++ display_code/bubble_sort_code/bubble_sort.py
@@ -1,14 +1,14 @@
-from colorama import Fore
+from tools.colorama_constants import *
 
 
 def display_bubble_sort_code():
     print(
         f"""
-{Fore.BLUE}def{Fore.WHITE} bubble_sort{Fore.YELLOW}({Fore.WHITE}initial_list{Fore.YELLOW}){Fore.WHITE}:
-    {Fore.BLUE}for{Fore.WHITE} i {Fore.BLUE}in{Fore.WHITE} range{Fore.YELLOW}({Fore.WHITE}len{Fore.YELLOW}({Fore.WHITE}initial_list{Fore.YELLOW}){Fore.WHITE} - 1{Fore.YELLOW}){Fore.WHITE}:
-        {Fore.BLUE}for{Fore.WHITE} j {Fore.BLUE}in{Fore.WHITE} range{Fore.YELLOW}({Fore.WHITE}len{Fore.YELLOW}({Fore.WHITE}initial_list{Fore.YELLOW}){Fore.WHITE} - 1{Fore.YELLOW}){Fore.WHITE}:
-            {Fore.BLUE}if{Fore.WHITE} initial_list{Fore.YELLOW}[{Fore.WHITE}j{Fore.YELLOW}]{Fore.WHITE} > initial_list{Fore.YELLOW}[{Fore.WHITE}j + 1{Fore.YELLOW}]{Fore.WHITE}:
-                initial_list{Fore.YELLOW}[{Fore.WHITE}j{Fore.YELLOW}]{Fore.WHITE}, initial_list{Fore.YELLOW}[{Fore.WHITE}j + 1{Fore.YELLOW}]{Fore.WHITE} = initial_list{Fore.YELLOW}[{Fore.WHITE}j + 1{Fore.YELLOW}]{Fore.WHITE}, initial_list{Fore.YELLOW}[{Fore.WHITE}j{Fore.YELLOW}]{Fore.WHITE}
-    {Fore.BLUE}return{Fore.WHITE} initial_list
+{blue}def{white} bubble_sort{yellow}({white}initial_list{yellow}){white}:
+    {blue}for{white} i {blue}in{white} range{yellow}({white}len{yellow}({white}initial_list{yellow}){white} - 1{yellow}){white}:
+        {blue}for{white} j {blue}in{white} range{yellow}({white}len{yellow}({white}initial_list{yellow}){white} - 1{yellow}){white}:
+            {blue}if{white} initial_list{yellow}[{white}j{yellow}]{white} > initial_list{yellow}[{white}j + 1{yellow}]{white}:
+                initial_list{yellow}[{white}j{yellow}]{white}, initial_list{yellow}[{white}j + 1{yellow}]{white} = initial_list{yellow}[{white}j + 1{yellow}]{white}, initial_list{yellow}[{white}j{yellow}]{white}
+    {blue}return{white} initial_list
     """
     )
