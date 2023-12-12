0c4dd23c3a7da337935e5c6004f69684d58fb33c
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-11 20:14:32 -0600

    chore: set up separate verables for colors

--- quick_sort.py
+++ quick_sort.py
@@ -2,6 +2,14 @@ from print_items import *
 from colorama import Fore
 
 
+white = Fore.WHITE
+green = Fore.GREEN
+blue = Fore.BLUE
+yellow = Fore.YELLOW
+magenta = Fore.MAGENTA
+cyan = Fore.CYAN
+
+
 def quick_sort(unsorted_list, constants, temp_list):
     """
     Sorts a list of numbers using quick sort algorithm, recursively
@@ -18,7 +26,7 @@ def quick_sort(unsorted_list, constants, temp_list):
         input("Press Press Enter to continue...")
         print("Entering quick sort")
         pivot = unsorted_list[0]
-        print(f"""{Fore.GREEN}Pivot: {pivot}{Fore.WHITE}""")
+        print(f"""{green}Pivot: {pivot}{white}""")
         if len(unsorted_list) > 0:
             
             print_list_with_pivot(unsorted_list, pivot, constants)
@@ -27,24 +35,38 @@ def quick_sort(unsorted_list, constants, temp_list):
                 print()
         less_than_pivot = [x for x in unsorted_list[1:] if x <= pivot]
         if len(less_than_pivot) > 0:
-            print(f"""Less than pivot: {Fore.YELLOW}{less_than_pivot}{Fore.WHITE}""")
+            print(
+                f"""Less than pivot: {yellow}{less_than_pivot}{white}"""
+            )
             print_items(less_than_pivot, constants)
         else:
-            print(f"""Less than pivot: {Fore.YELLOW}{less_than_pivot}{Fore.WHITE}""")
+            print(
+                f"""Less than pivot: {yellow}{less_than_pivot}{white}"""
+            )
         greater_than_pivot = [x for x in unsorted_list[1:] if x > pivot]
         if len(greater_than_pivot) > 0:
-            print(f"""Greater than pivot: {Fore.MAGENTA}{greater_than_pivot}{Fore.WHITE}""")
+            print(
+                f"""Greater than pivot: {magenta}{greater_than_pivot}{white}"""
+            )
             print_items(greater_than_pivot, constants)
         else:
-            print(f"""Greater than pivot: {Fore.MAGENTA}{greater_than_pivot}{Fore.WHITE}""")
-        merged_list = get_whole_list(less_than_pivot, pivot, greater_than_pivot)
+            print(
+                f"""Greater than pivot: {magenta}{greater_than_pivot}{white}"""
+            )
+        merged_list = get_whole_list(
+            less_than_pivot,
+            pivot,
+            greater_than_pivot
+        )
         if len(merged_list) > 0:
             input("Press Enter to see the merged list")
             print()
-            print(f"""Merged list: {Fore.CYAN}{merged_list}{Fore.WHITE}""")
+            print(f"""Merged list: {cyan}{merged_list}{white}""")
             print_items(merged_list, constants)
             print()
-        return quick_sort(less_than_pivot, constants, temp_list) + [pivot] + quick_sort(greater_than_pivot, constants, temp_list)
+        return quick_sort(
+            less_than_pivot, constants, temp_list
+        ) + [pivot] + quick_sort(greater_than_pivot, constants, temp_list)
 
 
 def print_list_with_pivot(unsorted_list, pivot, constants):
@@ -62,23 +84,23 @@ def print_list_with_pivot(unsorted_list, pivot, constants):
             if k != last_index:
                 if unsorted_list[k] == pivot:
                     print(
-                        f"""{Fore.GREEN}{unsorted_list[k]}{Fore.WHITE}""",
+                        f"""{green}{unsorted_list[k]}{white}""",
                         end=", "
                     )
                 else:
                     print(
-                        f"""{Fore.BLUE}{unsorted_list[k]}{Fore.WHITE}""",
+                        f"""{blue}{unsorted_list[k]}{white}""",
                         end=", "
                     )
             else:
                 if unsorted_list[k] == pivot:
                     print(
-                        f"""{Fore.GREEN}{unsorted_list[k]}{Fore.WHITE}""",
+                        f"""{green}{unsorted_list[k]}{white}""",
                         end=""
                     )
                 else:
                     print(
-                        f"""{Fore.BLUE}{unsorted_list[k]}{Fore.WHITE}""",
+                        f"""{blue}{unsorted_list[k]}{white}""",
                         end=""
                     )
     print("]")
