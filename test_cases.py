53a6acbac4a8c4ffd6bb0ac4be469c7d7e81226d
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-13 10:03:54 -0600

    feat: add case function for quicksort

--- test_cases.py
+++ test_cases.py
@@ -2,13 +2,15 @@ import random
 from merge_sort import merge_sort
 from selection_sort import selection_sort
 from animations.selection_sort_animation.selection_sort import *
-from print_items import print_items
+from tools.print_items import print_items
 from animations.merge_sort_animation.merge_sort import *
 from insertion_sort import insertion_sort
 from animations.insertion_sort_animation.insertion_sort import *
 from bubble_sort import bubble_sort
 from animations.bubble_sort_animation.bubble_sort import *
-from constant_dictionary import constants_1
+from quick_sort import quick_sort
+from tools.constant_dictionary import *
+from animations.quick_sort_animation.quick_sort import *
 
 
 def generate_unsorted_list_from_constants(constants, sort_algorithm_name):
@@ -152,3 +154,24 @@ def show_bubble_sort_animation_with_constants(constants):
     Initial list: {temp_list}
     """
     )
+
+
+def show_quick_sort_with_constants(constants):
+    """
+    Tests the merge sort algorithm with the constants dictionary
+    """
+    sort_algorithm_name = "QUICK SORT"
+    unsorted_list = generate_unsorted_list_from_constants(constants, sort_algorithm_name)
+    temp_list = unsorted_list.copy()
+    resulted_list = quick_sort(unsorted_list, constants, temp_list)
+    print("Final list: ")
+    print_items(resulted_list, constants)
+    print(
+        f"""
+
+    To compare:
+    Sorted list: {resulted_list}
+    Initial list: {temp_list}
+    """
+    )
+
