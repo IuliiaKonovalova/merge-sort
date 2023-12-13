3197a486d68aa3fc0cc8f6225be441481e8b6732
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-13 10:05:04 -0600

    feat: create separate module for handling printing graphics

---
+++ tools/print_items.py
@@ -0,0 +1,39 @@
+def print_items(keys, constants_dict):
+    """
+    Prints the items in a list
+    Args:
+        keys (list): list of numbers
+        constants_dict (dict): dictionary of constants
+    Returns:
+        None
+    """
+    max_key = max(keys)
+    if max_key == 0:
+        max_key = 1
+    for key in keys:
+        if key < 10:
+            print(" ", end="")
+        print(key, end=": ")
+        normalized_key = int(((key + 1) / max_key) * max_key)
+        color_code = constants_dict[key]
+        print(color_code * normalized_key)
+
+
+def print_temp_item(key, list_data, constants_dict):
+    """
+    Prints one item in a list
+    Args:
+        key (int): number
+        list_data (list): list of numbers
+        constants_dict (dict): dictionary of constants
+    Returns:
+        None
+    """
+    max_key = max(list_data)
+    if key == 0:
+        key = 1
+    if key < 10:
+        print(" ", end="")
+    print(key, end=": ")
+    normalized_key = int(((key + 1) / max_key) * max_key)
+    print(constants_dict[key] * normalized_key)
