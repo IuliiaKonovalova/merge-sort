d533baf572de9e412d269552cd95421af6013129
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-13 10:16:52 -0600

    restructure: remove generated unsorted list functionality

--- test_cases.py
+++ test_cases.py
@@ -1,8 +1,6 @@
-import random
 from merge_sort import merge_sort
 from selection_sort import selection_sort
 from animations.selection_sort_animation.selection_sort import *
-from tools.print_items import print_items
 from animations.merge_sort_animation.merge_sort import *
 from insertion_sort import insertion_sort
 from animations.insertion_sort_animation.insertion_sort import *
@@ -10,26 +8,8 @@ from bubble_sort import bubble_sort
 from animations.bubble_sort_animation.bubble_sort import *
 from quick_sort import quick_sort
 from tools.constant_dictionary import *
+from tools.unsorted_list_generation import *
 from animations.quick_sort_animation.quick_sort import *
-
-
-def generate_unsorted_list_from_constants(constants, sort_algorithm_name):
-    """
-    Generates an unsorted list of numbers
-    Args:
-        constants (dict): dictionary of constants
-        sort_algorithm_name (str): name of the sort algorithm
-    Returns:
-        unsorted_list (list): unsorted list of numbers
-    """
-    unsorted_list = []
-    for key in constants:
-        unsorted_list.append(key)
-    random.shuffle(unsorted_list)
-    print(sort_algorithm_name)
-    print(unsorted_list)
-    print_items(unsorted_list, constants)
-    return unsorted_list
 
 
 def show_merge_sort_with_constants(constants):
