9ea3ad52c02ec454e0320bbef98b8cd9ee163f24
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-14 09:16:27 -0600

    feat: add display code for algorithm functionality

---
+++ menu_feature/display_code_control.py
@@ -0,0 +1,25 @@
+from display_code.selection_sort_code.selection_sort import *
+from display_code.merge_sort_code.merge_sort import *
+from display_code.insertion_sort_code.insertion_sort import *
+from display_code.quick_sort_code.quick_sort import *
+from display_code.bubble_sort_code.bubble_sort import *
+
+
+def show_algorithm_code(algorithm_options_name):
+    """
+    Displays the code of the algorithm
+    Args:
+        algorithm_options_name (str): name of the algorithm
+    Returns:
+        None
+    """
+    if algorithm_options_name == 'Merge Sort':
+        display_merge_sort_code()
+    elif algorithm_options_name == 'Selection Sort':
+        display_selection_sort_code()
+    elif algorithm_options_name == 'Insertion Sort':
+        display_insertion_sort_code()
+    elif algorithm_options_name == 'Quick Sort':
+        display_quick_sort_code()
+    elif algorithm_options_name == 'Bubble Sort':
+        display_bubble_sort_code()
