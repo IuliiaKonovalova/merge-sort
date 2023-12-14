a947409067e539b55e1ef079797729cabd93933e
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-14 09:15:49 -0600

    feat: add list length options menu display functionality

---
+++ menu_feature/list_length_options_initialization.py
@@ -0,0 +1,121 @@
+from simple_term_menu import TerminalMenu
+from menu_feature.menu_constants import *
+from tools.constant_dictionary import *
+from test_cases import show_bubble_sort_animation_with_constants, show_bubble_sort_with_constants, show_insertion_sort_animation_with_constants, show_insertion_sort_with_constants, show_merge_sort_animation_with_constants, show_merge_sort_with_constants, show_quick_sort_animation_with_constants, show_quick_sort_with_constants, show_selection_sort_animation_with_constants, show_selection_sort_with_constants
+from display_code.merge_sort_code.merge_sort import display_merge_sort_code
+
+
+
+list_length_options_menu = TerminalMenu(list_length_options)
+
+def show_list_length_options_menu(algorithm_name, algorithm_option):
+    print(f"""{algorithm_name.upper()} - {algorithm_option.upper()}""")
+    list_length_options_index = list_length_options_menu.show()
+    list_length_options_choice = list_length_options[list_length_options_index]
+    if algorithm_name == 'Merge Sort':
+        if algorithm_option == 'animation':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_merge_sort_animation_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_merge_sort_animation_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_merge_sort_animation_with_constants(constants_3)
+        elif algorithm_option == 'step by step':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_merge_sort_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_merge_sort_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_merge_sort_with_constants(constants_3)
+        else:
+            display_merge_sort_code()
+    elif algorithm_name == 'Selection Sort':
+        if algorithm_option == 'animation':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_selection_sort_animation_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_selection_sort_animation_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_selection_sort_animation_with_constants(constants_3)
+        elif algorithm_option == 'step by step':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_selection_sort_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_selection_sort_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_selection_sort_with_constants(constants_3)
+    elif algorithm_name == 'Insertion Sort':
+        if algorithm_option == 'animation':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_insertion_sort_animation_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_insertion_sort_animation_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_insertion_sort_animation_with_constants(constants_3)
+        elif algorithm_option == 'step by step':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_insertion_sort_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_insertion_sort_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_insertion_sort_with_constants(constants_3)
+    elif algorithm_name == 'Quick sort':
+        if algorithm_option == 'animation':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_quick_sort_animation_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_quick_sort_animation_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_quick_sort_animation_with_constants(constants_3)
+        elif algorithm_option == 'step by step':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_quick_sort_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_quick_sort_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_quick_sort_with_constants(constants_3)
+    elif algorithm_name == 'Bubble sort':
+        if algorithm_option == 'animation':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_bubble_sort_animation_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_bubble_sort_animation_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_bubble_sort_animation_with_constants(constants_3)
+        elif algorithm_option == 'step by step':
+            if list_length_options_choice == '1. Yellow to red':
+                print(f'''YELLOW TO RED''')
+                show_bubble_sort_with_constants(constants_1)
+            elif list_length_options_choice == '2. Blue - White - Black':
+                print(f'''SHORT VERSION''')
+                show_bubble_sort_with_constants(constants_2)
+            elif list_length_options_choice == '3. Full version':
+                print(f'''FULL VERSION''')
+                show_bubble_sort_with_constants(constants_3)
