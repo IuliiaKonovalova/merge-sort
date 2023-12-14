95c04197c2a1971d0944e0000d932eebc7be67eb
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-14 09:14:02 -0600

    feat: add menu constants

---
+++ menu_feature/menu_constants.py
@@ -0,0 +1,18 @@
+options = [
+    'Merge Sort',
+    'Selection Sort',
+    'Insertion Sort',
+    'Bubble Sort',
+    'Quick Sort',
+    'Quit'
+]
+algorithm_options = [
+    '1. Animation',
+    '2. Step by step',
+    '3. Code',
+]
+list_length_options = [
+    '1. Yellow to red',
+    '2. Blue - White - Black',
+    '3. Full version',
+]
