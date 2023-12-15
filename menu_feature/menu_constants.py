d27d3b1008a235eb26c8696db01cccfde83f3362
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-15 14:53:56 -0600

    chore: format code, fix names options

--- menu_feature/menu_constants.py
+++ menu_feature/menu_constants.py
@@ -1,16 +1,23 @@
+""" This file contains all the constants used in the menu feature. """
+
+
 options = [
     'Merge Sort',
     'Selection Sort',
     'Insertion Sort',
-    'Bubble Sort',
     'Quick Sort',
+    'Bubble Sort',
     'Quit'
 ]
+
+
 algorithm_options = [
     '1. Animation',
     '2. Step by step',
     '3. Code',
 ]
+
+
 list_length_options = [
     '1. Yellow to red',
     '2. Blue - White - Black',
