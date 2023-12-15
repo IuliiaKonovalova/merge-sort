1fced53775141dd61956821aadcc83804c422593
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-15 14:59:08 -0600

    chore: delete unused code

--- menu_feature/menu_functionality.py
+++ menu_feature/menu_functionality.py
@@ -1,16 +1,7 @@
 
 from menu_feature.main_menu_initialization import *
 from menu_feature.algorithms_options_menu_initialization import *
-
-# from menu_feature.menu_constants import *
 from tools.constant_dictionary import *
-# from test_cases import *
-# from display_code.merge_sort_code.merge_sort import *
-# from display_code.selection_sort_code.selection_sort import *
-# from display_code.insertion_sort_code.insertion_sort import *
-# from display_code.bubble_sort_code.bubble_sort import *
-# from display_code.quick_sort_code.quick_sort import *
-from test_cases import show_bubble_sort_animation_with_constants, show_bubble_sort_with_constants, show_insertion_sort_animation_with_constants, show_insertion_sort_with_constants, show_merge_sort_animation_with_constants, show_merge_sort_with_constants, show_quick_sort_animation_with_constants, show_quick_sort_with_constants, show_selection_sort_animation_with_constants, show_selection_sort_with_constants
 
 
 def menu_logic():
