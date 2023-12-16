c50d15111f3cc8c8697c0637a3ced950f252af51
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-16 00:24:58 -0600

    chore: fix print statements, headings

--- animations/merge_sort_animation/split_functionality.py
+++ animations/merge_sort_animation/split_functionality.py
@@ -1,6 +1,7 @@
 import os
 from time import sleep
 from tools.print_items import print_items
+from tools.colorama_constants import *
 
 
 def split_list(initial_list, constants):
@@ -13,23 +14,28 @@ def split_list(initial_list, constants):
         right_part (list): right part of the list
     """
     os.system('clear')
-    print("MERGE SORT ANIMATION\n")
+    print(
+      f"""{yellow}MERGE SORT ANIMATION{white}
+      """
+    )
     print("TO BE SPLIT: ", initial_list)
     print_items(initial_list, constants)
+    print("\n")
     sleep(1.5)
 
     mid_point = len(initial_list) // 2
     left_part = initial_list[:mid_point]
-    print("\n LEFT: ", left_part)
-
+    print("LEFT: ", left_part)
     print_items(left_part, constants)
     print("\n")
     right_part = initial_list[mid_point:]
-    print("\n RIGHT: ", right_part)
+    print("RIGHT: ", right_part)
     print_items(right_part, constants)
-    print("\n")
     sleep(1.5)
     os.system('clear')
-    print("MERGE SORT ANIMATION\n")
+    print(
+      f"""{yellow}MERGE SORT ANIMATION{white}
+      """
+    )
 
     return left_part, right_part
