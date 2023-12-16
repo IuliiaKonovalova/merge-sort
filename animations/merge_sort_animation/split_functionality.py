23403b474b7920a2c44b916ee7207cbbe4ec8cfa
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-15 18:14:46 -0600

    feat: add to be splited list and sleep functionality

--- animations/merge_sort_animation/split_functionality.py
+++ animations/merge_sort_animation/split_functionality.py
@@ -1,5 +1,5 @@
-from time import sleep
 import os
+from time import sleep
 from tools.print_items import print_items
 
 
@@ -12,6 +12,10 @@ def split_list(initial_list, constants):
         left_part (list): left part of the list
         right_part (list): right part of the list
     """
+    os.system('clear')
+    print("\n TO BE SPLIT: ", initial_list)
+    print_items(initial_list, constants)
+    sleep(1.5)
 
     mid_point = len(initial_list) // 2
     left_part = initial_list[:mid_point]
@@ -23,7 +27,7 @@ def split_list(initial_list, constants):
     print("\n RIGHT: ", right_part)
     print_items(right_part, constants)
     print("\n")
-    sleep(1)
+    sleep(1.5)
     os.system('clear')
 
     return left_part, right_part
