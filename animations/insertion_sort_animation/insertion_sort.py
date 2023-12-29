9cae33fd0f8712623ad02ab8b119f285f9cbd074
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-29 08:01:05 -0600

    chore: fix formatting

--- animations/insertion_sort_animation/insertion_sort.py
+++ animations/insertion_sort_animation/insertion_sort.py
@@ -1,4 +1,6 @@
-from animations.insertion_sort_animation.animation_control import animation_control
+from animations.insertion_sort_animation.animation_control import (
+    animation_control
+)
 
 
 def insertion_sort_animation(list_data, constants):
@@ -11,9 +13,20 @@ def insertion_sort_animation(list_data, constants):
     """
     for scan_index in range(1, len(list_data)):
         temp = list_data[scan_index]
-        animation_control(temp, list_data, constants, None, scan_index, in_progress=True)
+        animation_control(
+            temp,
+            list_data,
+            constants,
+            None,
+            scan_index,
+            in_progress=True
+        )
         min_index = scan_index
-        animation_control(temp, list_data, constants, min_index, scan_index, in_progress=True)
+        animation_control(
+            temp, list_data, constants, min_index,
+            scan_index,
+            in_progress=True
+        )
         while min_index > 0 and temp < list_data[min_index - 1]:
             list_data[min_index] = list_data[min_index - 1]
             min_index -= 1
@@ -26,6 +39,20 @@ def insertion_sort_animation(list_data, constants):
                 scan_index,
                 in_progress=True
             )
-        animation_control(temp, list_data, constants, min_index, scan_index, in_progress=True)
-    animation_control(temp, list_data, constants, min_index, scan_index, in_progress=False)
+        animation_control(
+            temp,
+            list_data,
+            constants,
+            min_index,
+            scan_index,
+            in_progress=True
+        )
+    animation_control(
+        temp,
+        list_data,
+        constants,
+        min_index,
+        scan_index,
+        in_progress=False
+    )
     return list_data
