031c380deb5e6b1570132ba7682b07ffa8e24851
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2024-01-15 15:34:58 -0600

    chore: add doc string

--- menu_feature/back_menu_functionality.py
+++ menu_feature/back_menu_functionality.py
@@ -3,6 +3,13 @@ from menu_feature.menu_constants import back_options
 
 
 def show_back_menu():
+    """
+    Displays the back menu
+    Args:
+        None
+    Returns:
+        None
+    """
     back_menu = TerminalMenu(back_options)
     back_menu_index = back_menu.show()
     back_menu_choice = back_options[back_menu_index]
