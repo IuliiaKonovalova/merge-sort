67c0008be0d8550b29000215ac8f2242f0d58d30
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2024-01-15 15:29:43 -0600

    chore: add welcome msg

--- run.py
+++ run.py
@@ -3,6 +3,14 @@ from menu_feature.menu_functionality import *
 
 def main():
     input("Hit enter to start the program...")
+    print(
+        f"""
+        Welcome to searching algorithm program!
+        This program will teach you how searching algorithms work.
+        You will be able to see how the algorithm animation, step by step, and 
+        you can also see the code of the algorithm.
+        """
+    )
     menu_logic()
 
 
