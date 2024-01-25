f1cf70463d52d15e881a1aea1f12042d6e2e5ec1
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2024-01-25 15:31:30 -0600

    chore: remove back to main menu functionality

--- display_code/insertion_sort_code/insertion_sort.py
+++ display_code/insertion_sort_code/insertion_sort.py
@@ -1,6 +1,5 @@
 from time import sleep
 from tools.painting_constants import *
-from menu_feature.back_menu_functionality import show_back_menu
 
 
 def display_insertion_sort_code():
@@ -18,5 +17,3 @@ def display_insertion_sort_code():
     {blue}return{white} list_data
     """
     )
-    sleep(2)
-    show_back_menu()
