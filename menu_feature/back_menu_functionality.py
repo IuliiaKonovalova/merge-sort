ef06a48278993232d4ae0aaa641b93de25200332
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2024-01-26 16:26:51 -0600

    chore: fix imports

--- menu_feature/back_menu_functionality.py
+++ menu_feature/back_menu_functionality.py
@@ -1,6 +1,6 @@
-from os import system
-from simple_term_menu import TerminalMenu
-from menu_feature.menu_constants import back_options
+from core_imports.core_imports import *
+from menu_feature.menu_constants import *
+from menu_feature.menu_functionality import menu_logic
 
 
 def show_back_menu():
@@ -15,7 +15,6 @@ def show_back_menu():
     back_menu_index = back_menu.show()
     back_menu_choice = back_options[back_menu_index]
     if back_menu_choice == '1. Back':
-        from menu_feature.menu_functionality import menu_logic
         system('clear')
         menu_logic()
     elif back_menu_choice == '2. Exit':
