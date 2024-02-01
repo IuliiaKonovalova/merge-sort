dda79d4ab10f2a3866686ded6bde5cbf78468a67
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2024-02-01 16:18:10 -0600

    fix: core menu logic bug

--- menu_feature/display_code_control.py
+++ menu_feature/display_code_control.py
@@ -1,8 +1,5 @@
-from display_code.selection_sort_code.selection_sort import *
-from display_code.merge_sort_code.merge_sort import *
-from display_code.insertion_sort_code.insertion_sort import *
-from display_code.quick_sort_code.quick_sort import *
-from display_code.bubble_sort_code.bubble_sort import *
+from core_imports.display_code_imports import *
+from menu_feature.back_menu_functionality import show_back_menu
 
 
 def show_algorithm_code(algorithm_options_name):
